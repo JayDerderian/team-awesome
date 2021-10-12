24c5b05a7587c86a8390541aff282c344e68174b
Michael Han <hanmic@pdx.edu>
2021-10-12 13:44:39 -0700

    Created basic unit test for politician

--- battlecode21-scaffolding/test/politician/RobotPlayerTest.java
+++ battlecode21-scaffolding/test/politician/RobotPlayerTest.java
@@ -1,4 +1,12 @@
 package politician;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class RobotPlayerTest {
+    @Test
+    public void testCanCreatePolitician() {
+        RobotPlayer test = new RobotPlayer();
+        assertNotNull(test);
+    }
 }
