41c3a1ae4a2b6d1057dab7ddf79f77ade78cb498
Michael Han <hanmic@pdx.edu>
2021-10-14 13:46:44 -0700

    Created politician move algorithm

--- battlecode21-scaffolding/src/politician/RobotPlayer.java
+++ battlecode21-scaffolding/src/politician/RobotPlayer.java
@@ -1,6 +1,9 @@
 package politician;
 import battlecode.common.*;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class RobotPlayer {
 
     static RobotController rc;
@@ -15,5 +18,44 @@ public class RobotPlayer {
             System.out.println("empowered");
             return;
         }
+        // then try to move
+        MapLocation next = mostPassable();
+        Direction d = next.directionTo(rc.getLocation());
+        if(rc.canMove(d)) {
+            rc.move(d);
+            System.out.println("I Moved! Direction: " + d.toString());
+        } else {
+            System.out.println("I'm Stuck!");
+        }
+    }
+
+    /*
+    Find the most passable square
+     */
+    public MapLocation mostPassable() throws GameActionException {
+        int[] x = {-1, 0, 1};
+        int[] y = {-1, 0, 1};
+        MapLocation currentLocation = rc.getLocation();
+        LinkedList<MapLocation> locations = new LinkedList<>();
+        for(int xi : x) {
+            for(int yi : y) {
+                if(xi == 0 && yi == 0) {
+                    // do nothing
+                } else {
+                    locations.add(new MapLocation(currentLocation.x + xi, currentLocation.y + yi));
+                }
+            }
+        }
+        MapLocation toReturn = locations.get(0);
+        double maxPass = rc.sensePassability(toReturn);;
+        for (MapLocation m:
+             locations) {
+            double thisPass = rc.sensePassability(m);
+            if(thisPass > maxPass) {
+                maxPass = thisPass;
+                toReturn = m;
+            }
+        }
+        return toReturn;
     }
 }
