955d26b11db04e2778b120ff6c7b31c7e203f23d
Sree-Vandana <sreevandana.sv@gmail.com>
2021-10-18 18:07:30 -0700

    modified muckraker code, can scence neutral EC, or scence EC and sladered from team members and move toward sladeres

--- battlecode21-scaffolding/src/TeamAwsomeSprint1/FlagConstants.java
+++ battlecode21-scaffolding/src/TeamAwsomeSprint1/FlagConstants.java
@@ -4,6 +4,6 @@ public interface FlagConstants {
 
     static final int NEUTRAL_ENLIGHTENMENT_CENTER_FLAG = 50;
 
-    static final int SLANDERER_FLAG = 102;
+    static final int ENEMY_SLANDERER_FLAG = 102;
 
 }
