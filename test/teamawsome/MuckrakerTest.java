7a3e4c01f2cceaf2a9968c210b193977d3e26ee4
Sree-Vandana <sreevandana.sv@gmail.com>
2021-10-30 14:20:34 -0700

    refactored Muckraker unit tests, removed comments and unused code (code cleaning)

--- test/teamawsome/MuckrakerTest.java
+++ test/teamawsome/MuckrakerTest.java
@@ -1,25 +1,13 @@
 package teamawsome;
 
 import battlecode.common.*;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-
-import teamawesome.GenericRobot;
-import teamawesome.Muckraker;
-import teamawesome.RobotPlayer;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.util.ArrayList;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
+import teamawesome.Muckraker;
 
 /**
  * Constants Meaning
@@ -28,45 +16,33 @@ import static org.mockito.Mockito.*;
  * 3. new MapLocation(int x, int y)
  */
 public class MuckrakerTest {
+
     RobotInfo enemy1 = new RobotInfo(1, Team.B, RobotType.SLANDERER, 1, 1, new MapLocation(20000, 20000));
     RobotInfo enemy2 = new RobotInfo(2, Team.B, RobotType.SLANDERER, 1, 1, new MapLocation(20000, 20000));
     RobotInfo enemy3 = new RobotInfo(3, Team.B, RobotType.SLANDERER, 1, 1, new MapLocation(20000, 20000));
     RobotInfo[] enemyRobotInfoArray = {enemy1, enemy2, enemy3};
 
     @Test
-    public void testSanity() {
-        assertEquals(2, 1+1);
-    }
-
-    @Test
-    public void test1() throws GameActionException {
-        RobotPlayer rp = new RobotPlayer();
-        RobotController rc = mock(RobotController.class);
-        when(rc.getType()).thenReturn(RobotType.MUCKRAKER); // stubbing = assigning a value
-    }
-
-    @Test
-    public void ifMuckrakerRobotCreatedThenMuckrakerClassIsCalled() throws GameActionException {
-        RobotPlayer rp = new RobotPlayer();
+    public void ifMuckrakerRobotCreatedThenMuckrakerClassIsCalled() {
         RobotController rc = mock(RobotController.class);
         when(rc.getType()).thenReturn(RobotType.MUCKRAKER);
-//        GenericRobot robot = mock(GenericRobot.class);
-//        when(robot).thenReturn(new Muckraker(rc));
-        GenericRobot robot = new Muckraker(rc);
-//        assertEquals("I'm a Muckraker", ((Muckraker) robot).robotStatement);
-        assertThat(((Muckraker) robot).robotStatement, containsString("I'm a MUCKRAKER"));
+
+        Muckraker robot = new Muckraker(rc);
+
+        assertThat(robot.robotStatement, containsString("I'm a MUCKRAKER"));
     }
 
     @Test
     public void ifEnemyRobotSensedAndCanBeExposeThenExpose() throws GameActionException {
         RobotController rc = mock(RobotController.class);
         when(rc.getType()).thenReturn(RobotType.MUCKRAKER);
-        GenericRobot robot = new Muckraker(rc);
-        when(rc.getTeam()).thenReturn(Team.A); // this robots team=A
-//        when(rc.getTeam().opponent()).thenReturn(Team.B); // opponent=B
+        when(rc.getTeam()).thenReturn(Team.A);
         when(rc.senseNearbyRobots()).thenReturn(enemyRobotInfoArray);
         when(rc.canExpose(new MapLocation(20000, 20000))).thenReturn(true);
-        ((Muckraker) robot).turn();
+
+        Muckraker robot = new Muckraker(rc);
+        robot.turn();
+
         assertEquals(enemyRobotInfoArray[0].getTeam(), Team.B);
         assertTrue(rc.canExpose(new MapLocation(20000, 20000)));
     }
