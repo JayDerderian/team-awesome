f36a6d3664b0363c14d61eee30e319dfbd71f579
Sree-Vandana <sreevandana.sv@gmail.com>
2021-11-11 13:57:11 -0800

    mucks move toward enemy camp, need to chnage flag conv to dir num

--- src/teamawesome/Muckraker.java
+++ src/teamawesome/Muckraker.java
@@ -25,7 +25,10 @@ public strictfp class Muckraker extends GenericRobot {
     ArrayList<Direction> zigZagDown = new ArrayList<Direction>(Arrays.asList(Direction.SOUTH, Direction.WEST, Direction.NORTH));
     int zigZagIndex = 0;
     boolean enemyEcFound = false;
-
+    boolean hasPrevMovedDir = false;
+    Direction prevMovedDir;
+    boolean botDirectionToMoveSet = false;
+    Direction botDirectionToMove;
 
     /**
      * constructor
@@ -33,15 +36,6 @@ public strictfp class Muckraker extends GenericRobot {
      */
     public Muckraker(RobotController newRc) {
         super(newRc);
-//        mapDirectionNum.put(Direction.NORTH, 0);
-//        mapDirectionNum.put(Direction.EAST, 0);
-//        mapDirectionNum.put(Direction.SOUTH, 0);
-//        mapDirectionNum.put(Direction.WEST, 0);
-//        mapDirectionNum.put(Direction.CENTER, 0); // map.get(key)
-//        mapDirectionNum.put(Direction.NORTHEAST, 0);
-//        mapDirectionNum.put(Direction.NORTHWEST, 0);
-//        mapDirectionNum.put(Direction.SOUTHEAST, 0);
-//        mapDirectionNum.put(Direction.SOUTHWEST, 0);
     }
 
     /**
@@ -55,158 +49,89 @@ public strictfp class Muckraker extends GenericRobot {
         Team enemy = rc.getTeam().opponent();
         enemyEcFound = false;
 
-        RobotInfo[] sensedNearByRobots =  rc.senseNearbyRobots();
+        RobotInfo[] sensedNearByRobots = rc.senseNearbyRobots();
 
-        while(!enemyEcFound){
-            for (RobotInfo robot : sensedNearByRobots) {
-                // ENEMY
-                if(robot.getTeam() == enemy) {
-                    if (robot.type.canBeExposed()) {
-                        // It's a slanderer... go get them!
-                        if (rc.canExpose(robot.location)) {
-                            exposedSuccess = true;
-                            System.out.println("e x p o s e d");
-                            rc.expose(robot.location);
-                            return;
-                        }
-                    }
-                    if(robot.type == RobotType.ENLIGHTENMENT_CENTER){
-                        enemyEcFound = true;
-                        enemyECLocation = robot.location;
-                        enemyECDirection = robot.location.directionTo(enemyECLocation);
-                        break;
+        for (RobotInfo robot : sensedNearByRobots) {
+            // ENEMY
+            if (robot.getTeam() == enemy) {
+                if (robot.type.canBeExposed()) {
+                    // It's a slanderer... go get them!
+                    if (rc.canExpose(robot.location)) {
+                        exposedSuccess = true;
+                        System.out.println("e x p o s e d");
+                        rc.expose(robot.location);
+                        return;
                     }
                 }
-            }
-
-            if(enemyEcFound)
-                break;
-
-            int stepCount = 1;
-            while (tryMove(zigZagUp.get(zigZagIndex))){
-                System.out.println("I moved!");
-                stepCount += 1;
-                if(stepCount == 30)
+                if (robot.type == RobotType.ENLIGHTENMENT_CENTER) {
+                    enemyEcFound = true;
+                    enemyECLocation = robot.location;
+                    enemyECDirection = robot.location.directionTo(enemyECLocation);
+                    // set Flag to let other muck's know
+                    int flagValue = makeFlag(ENEMY_ENLIGHTENMENT_CENTER_FLAG, 0);
+                    if (rc.canSetFlag(flagValue))
+                        rc.setFlag(flagValue);
+                    botDirectionToMove = enemyECDirection;
+                    botDirectionToMoveSet = true;
                     break;
-            }
-            sensedNearByRobots =  rc.senseNearbyRobots();
-            if(!rc.onTheMap(rc.adjacentLocation(zigZagUp.get(zigZagIndex + 1 % zigZagUp.size()))))
-                zigZagIndex = zigZagIndex+1 % zigZagUp.size();
-        }
-        if(enemyEcFound){
-            // move to its adjacent location
-            while (rc.adjacentLocation(enemyECDirection) != enemyECLocation) {
-                if (tryMove(enemyECDirection)) {
-                    System.out.println("I moved!");
-                } else if (tryMove(randomDirection())) {
-                    System.out.println("I moved!");
                 }
-            }
-
-            if(rc.adjacentLocation(enemyECDirection) == enemyECLocation){
-                sensedNearByRobots =  rc.senseNearbyRobots();
-                for (RobotInfo robot : sensedNearByRobots) {
-                    // ENEMY
-                    if(robot.getTeam() == enemy) {
-                        if (robot.type.canBeExposed()) {
-                            // It's a slanderer... go get them!
-                            if (rc.canExpose(robot.location)) {
-                                exposedSuccess = true;
-                                System.out.println("e x p o s e d");
-                                rc.expose(robot.location);
-                                return;
-                            }
-                        }
+            } else if (robot.getTeam() != enemy){
+                if(rc.canGetFlag(robot.ID)){
+                    int flagValue = rc.getFlag(robot.ID);
+                    if(flagValue == 1140 && !botDirectionToMoveSet){
+                        botDirectionToMove = robot.location.directionTo(robot.getLocation());
+                        botDirectionToMoveSet = true;
                     }
                 }
             }
-
         }
+
+        // Move
+        if(botDirectionToMoveSet){
+            if(tryMove(botDirectionToMove)) {
+                System.out.println("Muck Moved!");
+                hasPrevMovedDir = true;
+                prevMovedDir = botDirectionToMove;
+            } else {
+                Direction nextRandom = randomDirection();
+                if (tryMove(nextRandom)) {
+                    System.out.println("Muck moved!");
+                    hasPrevMovedDir = true;
+                    prevMovedDir = nextRandom;
+                }
+            }
+        } else {
+            Direction leastPassabilityDirection = getLeastPassableDirection();
+            if(tryMove(leastPassabilityDirection)) {
+                hasPrevMovedDir = true;
+                prevMovedDir = leastPassabilityDirection;
+                System.out.println("Muck Moved!");
+            } else {
+                Direction nextRandom = randomDirection();
+                if (tryMove(nextRandom)) {
+                    System.out.println("Muck moved!");
+                    hasPrevMovedDir = true;
+                    prevMovedDir = nextRandom;
+                }
+            }
+        }
+
+        botDirectionToMoveSet = false;
+
     }
 
-
-//    public void turn() throws GameActionException {
-//        homeECLocation = rc.getLocation();
-//
-//        System.out.println(robotStatement);
-//
-//        Team enemy = rc.getTeam().opponent();
-//        boolean DetectEnemySlanderer = false;
-//        Direction detectedDirection = Direction.CENTER; // random value, change later
-//        RobotInfo[] sensedNearByRobots =  rc.senseNearbyRobots();
-//
-//        // 1. Sense Every Robot (max actionRadiusSquared)
-//        for (RobotInfo robot : sensedNearByRobots) {
-//            exposedSuccess = false;
-////            nextMoveDirSet = false;
-//            // ENEMY
-//            if(robot.getTeam() == enemy){
-//                if (robot.type.canBeExposed()) {
-//                    // It's a slanderer... go get them!
-//                    Direction enemySlandererDir = robot.location.directionTo(robot.location);
-//                    mapDirectionNum.computeIfPresent(enemySlandererDir, (k, v) -> v + 1);
-//                    if (rc.canExpose(robot.location)) {
-//                        exposedSuccess = true;
-//                        System.out.println("e x p o s e d");
-//                        rc.expose(robot.location);
-//                        return;
-//                    }
-//                } else if (robot.getType() == RobotType.ENLIGHTENMENT_CENTER) { // Enemy enlightenment center
-//                    Direction enemyECDir = robot.location.directionTo(robot.getLocation());
-//                    mapDirectionNum.computeIfPresent(enemyECDir, (k, v) -> v + 2);
-//                }
-//            }
-//            // NEUTRAL EC
-//            if(robot.getTeam() == Team.NEUTRAL) {
-//                neutralLocation = robot.getLocation();
-//                // generate flag and set flag
-//                if(rc.canSetFlag(NEUTRAL_ENLIGHTENMENT_CENTER_FLAG))
-//                    rc.setFlag(NEUTRAL_ENLIGHTENMENT_CENTER_FLAG);
-//            }
-//            // OUR TEAM ROBOT
-//            else if(robot.getTeam() != enemy) {
-//                if (rc.canGetFlag(robot.getID())){
-//                    // Get the Flag
-//                    flagSensed = rc.getFlag(robot.getID());
-//                    if(robot.getType() == RobotType.ENLIGHTENMENT_CENTER){
-//                        // decode what our EC is saying.
-//                    }
-//                    // Decode the Flag
-//                    // yet to implement...
-//                    // Flag about Neutral EC
-//                    // Flag about Enemy Slanderer
-//                    if(flagSensed == ENEMY_SLANDERER_NEARBY_FLAG){
-//                        DetectEnemySlanderer = true;
-//                        // decode their location
-//                        // set own flag value
-//                    }
-//                }
-//            }
-//        }
-//
-////        for (int i = 0; i < zigZagUp.size(); i=i%zigZagUp.size()-1){
-////            while (tryMove(zigZagUp.get(i))){
-////            System.out.println("I moved!");
-////            if(sensedNearByRobots)
-////            }
-////        }
-//
-//    }
-
-//    private void resetMapDirNum() {
-//        mapDirectionNum.replaceAll((k,v) -> 0);
-//    }
-
-//    private Direction getPopularMapDirection() {
-//        int max = Collections.max(mapDirectionNum.values());
-//        if(max !=0){
-//            for (Map.Entry<Direction, Integer> entry : mapDirectionNum.entrySet()) {
-//                if (entry.getValue()==max) {
-//                    return entry.getKey();
-//                }
-//            }
-//        }
-//        return randomDirection();
-//    }
-
+    private Direction getLeastPassableDirection() throws GameActionException {
+        double minPass = 1.0;
+        Direction minPassDir = randomDirection();
+        for(Direction d: Direction.values()){
+            if(hasPrevMovedDir && prevMovedDir.opposite() != d){
+                double currPass = rc.sensePassability(rc.adjacentLocation(d));
+                if(currPass < minPass){
+                    minPass = currPass;
+                    minPassDir = d;
+                }
+            }
+        }
+        return minPassDir;
+    }
 }
