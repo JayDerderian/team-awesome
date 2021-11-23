615f24da9a0a908e55c8280ae3731ff13bf69a94
madisen-hallberg <76964032+madisen-hallberg@users.noreply.github.com>
2021-11-23 13:43:13 -0800

    Test changes

---
+++ codemr/team-awesome 4/html/main_report/htmlx/modeldata/sunburst_data.js
@@ -0,0 +1,167 @@
+function EQ_GET_DATA(){ 
+	  var ret = {
+"name": " team-awesome", "value":1136, 
+"prmetrics":{"5":1,"6":1,"7":1,"8":2,"9":1,"10":1,"11":1},
+"prmetricvalues":{"5":0,"6":12,"7":0,"8":1136,"9":5,"10":6,"11":33},
+"children": [ {
+"name": "muckslan", "key": "dX", "value":113, 
+"pmetrics":{"4":1,"12":1,"13":1,"14":1,"3":1,"1":1,"0":1,"6":1,"8":1,"2":1,"15":1,"16":1,"17":1},
+"pmetricvalues":{"4":1,"12":1,"13":0,"14":1,"3":1,"1":1,"0":1,"18":0.0,"6":1,"19":0.0,"8":113,"20":1.0,"2":1,"15":0,"16":40,"21":0,"17":0,"22":0},
+"children":[
+{
+"name": "RobotPlayer","key": "dY","value":113, 
+"metrics":{"23":2,"24":1,"25":1,"26":1,"27":2,"28":1,"29":1,"0":2,"30":1,"31":2,"32":1,"33":4,"34":1,"35":1,"36":1,"37":1,"38":1,"39":2,"16":2,"40":1,"4":2,"8":2,"2":2,"3":1,"1":2},
+"metricvalues":{"23":8,"24":0,"25":0,"26":0.0,"27":92,"28":0,"29":1,"0":2,"30":0,"31":84,"32":0.0,"33":0.857,"34":7,"35":0.593,"36":1,"37":40,"38":9,"39":7,"16":40,"40":0,"4":2,"8":113,"2":2,"3":1,"1":2}
+}
+]
+},{
+"name": "stratx_han", "key": "dZ", "value":112, 
+"pmetrics":{"4":1,"12":1,"13":1,"14":1,"3":1,"1":1,"0":1,"6":1,"8":1,"2":1,"15":1,"16":1,"17":1},
+"pmetricvalues":{"4":1,"12":1,"13":0,"14":1,"3":1,"1":1,"0":1,"18":0.0,"6":1,"19":0.0,"8":112,"20":1.0,"2":1,"15":0,"16":39,"21":0,"17":0,"22":0},
+"children":[
+{
+"name": "RobotPlayer","key": "d0","value":112, 
+"metrics":{"23":2,"24":1,"25":1,"26":1,"27":2,"28":1,"29":1,"0":2,"30":1,"31":2,"32":1,"33":4,"34":1,"35":1,"36":1,"37":1,"38":1,"39":2,"16":2,"40":1,"4":2,"8":2,"2":2,"3":1,"1":2},
+"metricvalues":{"23":8,"24":0,"25":0,"26":0.0,"27":91,"28":0,"29":1,"0":2,"30":0,"31":94,"32":0.0,"33":0.878,"34":7,"35":0.583,"36":2,"37":32,"38":8,"39":6,"16":39,"40":0,"4":2,"8":112,"2":2,"3":1,"1":2}
+}
+]
+},{
+"name": "sland_rush", "key": "d1", "value":89, 
+"pmetrics":{"4":1,"12":1,"13":1,"14":1,"3":1,"1":1,"0":1,"6":1,"8":1,"2":1,"15":1,"16":1,"17":1},
+"pmetricvalues":{"4":1,"12":1,"13":0,"14":1,"3":1,"1":1,"0":1,"18":0.0,"6":1,"19":0.0,"8":89,"20":1.0,"2":1,"15":0,"16":28,"21":0,"17":0,"22":0},
+"children":[
+{
+"name": "RobotPlayer","key": "d2","value":89, 
+"metrics":{"23":2,"24":1,"25":1,"26":1,"27":2,"28":1,"29":1,"0":2,"30":1,"31":1,"32":1,"33":4,"34":1,"35":1,"36":1,"37":1,"38":1,"39":2,"16":2,"40":1,"4":2,"8":2,"2":2,"3":1,"1":2},
+"metricvalues":{"23":6,"24":0,"25":0,"26":0.0,"27":70,"28":0,"29":1,"0":2,"30":0,"31":31,"32":0.0,"33":0.833,"34":5,"35":0.583,"36":0,"37":30,"38":8,"39":6,"16":28,"40":0,"4":2,"8":89,"2":2,"3":1,"1":2}
+}
+]
+},{
+"name": "muck_rush", "key": "dV", "value":96, 
+"pmetrics":{"4":1,"12":1,"13":1,"14":1,"3":1,"1":1,"0":1,"6":1,"8":1,"2":1,"15":1,"16":1,"17":1},
+"pmetricvalues":{"4":1,"12":1,"13":0,"14":1,"3":1,"1":1,"0":1,"18":0.0,"6":1,"19":0.0,"8":96,"20":1.0,"2":1,"15":0,"16":32,"21":0,"17":0,"22":0},
+"children":[
+{
+"name": "RobotPlayer","key": "dW","value":96, 
+"metrics":{"23":2,"24":1,"25":1,"26":1,"27":2,"28":1,"29":1,"0":2,"30":1,"31":1,"32":1,"33":4,"34":1,"35":1,"36":1,"37":1,"38":1,"39":2,"16":2,"40":1,"4":2,"8":2,"2":2,"3":1,"1":2},
+"metricvalues":{"23":7,"24":0,"25":0,"26":0.0,"27":77,"28":0,"29":1,"0":2,"30":0,"31":34,"32":0.0,"33":0.833,"34":5,"35":0.583,"36":0,"37":33,"38":8,"39":7,"16":32,"40":0,"4":2,"8":96,"2":2,"3":1,"1":2}
+}
+]
+},{
+"name": "teamawesome", "key": "j", "value":637, 
+"pmetrics":{"4":2,"12":2,"13":1,"14":2,"3":1,"1":2,"0":2,"6":2,"8":2,"2":2,"15":1,"16":2,"17":1},
+"pmetricvalues":{"4":2,"12":6,"13":1,"14":6,"3":1,"1":2,"0":2,"18":0.036,"6":7,"19":0.286,"8":637,"20":0.75,"2":2,"15":2,"16":219,"21":522,"17":12,"22":62},
+"children":[
+{
+"name": "FlagConstants","key": "l","value":16, 
+"metrics":{"23":1,"24":1,"25":1,"26":1,"27":1,"28":1,"29":1,"0":1,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":1,"37":1,"38":1,"39":1,"40":1,"4":1,"8":1,"2":1,"3":1,"16":1,"1":1},
+"metricvalues":{"23":0,"24":0,"25":0,"26":0.0,"27":0,"28":0,"29":1,"0":1,"30":0,"31":0,"32":0.0,"33":0.0,"34":15,"35":0.0,"36":0,"37":0,"38":0,"39":0,"40":0,"4":1,"8":16,"2":1,"3":1,"16":0,"1":1}
+},
+{
+"name": "Politician","key": "o","value":149, 
+"metrics":{"23":2,"24":1,"25":1,"26":1,"27":2,"28":1,"29":2,"0":2,"30":1,"31":2,"32":1,"33":4,"34":1,"35":2,"36":1,"37":1,"38":1,"39":2,"16":2,"40":1,"4":2,"8":2,"17":1,"2":2,"3":2,"1":2},
+"metricvalues":{"23":8,"24":0,"25":9,"26":0.0,"27":139,"28":0,"29":2,"0":2,"30":9,"31":61,"32":0.333,"33":0.861,"34":0,"35":0.685,"36":1,"37":48,"38":0,"39":7,"16":46,"40":0,"21":134,"4":2,"8":149,"17":0,"2":2,"3":2,"22":0,"1":2}
+},
+{
+"name": "EnlightenmentCenter","key": "k","value":69, 
+"metrics":{"23":2,"24":1,"25":1,"26":1,"27":2,"28":1,"29":2,"0":2,"30":1,"31":1,"32":1,"33":4,"34":1,"35":1,"36":1,"37":1,"38":1,"39":1,"16":2,"40":1,"4":2,"8":2,"17":1,"2":2,"3":1,"1":2},
+"metricvalues":{"23":6,"24":0,"25":2,"26":0.0,"27":65,"28":0,"29":2,"0":2,"30":3,"31":23,"32":0.0,"33":1.0,"34":1,"35":0.533,"36":2,"37":17,"38":2,"39":4,"16":31,"40":0,"21":50,"4":2,"8":69,"17":0,"2":2,"3":1,"22":0,"1":2}
+},
+{
+"name": "Slanderer","key": "s","value":99, 
+"metrics":{"23":2,"24":1,"25":1,"26":1,"27":2,"28":1,"29":2,"0":2,"30":1,"31":1,"32":2,"33":4,"34":1,"35":2,"36":1,"37":1,"38":1,"39":2,"16":2,"40":1,"4":2,"8":2,"17":1,"2":2,"3":2,"1":2},
+"metricvalues":{"23":6,"24":0,"25":6,"26":0.0,"27":83,"28":0,"29":2,"0":2,"30":8,"31":33,"32":0.607,"33":0.833,"34":0,"35":0.656,"36":0,"37":30,"38":0,"39":6,"16":38,"40":0,"21":81,"4":2,"8":99,"17":0,"2":2,"3":2,"22":0,"1":2}
+},
+{
+"name": "RobotPlayer","key": "p","value":43, 
+"metrics":{"23":2,"24":1,"25":1,"26":1,"27":1,"28":1,"29":1,"0":2,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":1,"37":1,"38":1,"39":1,"16":1,"40":1,"4":1,"8":1,"17":1,"2":2,"3":1,"1":1},
+"metricvalues":{"23":9,"24":0,"25":0,"26":0.0,"27":25,"28":0,"29":1,"0":2,"30":0,"31":8,"32":0.0,"33":0.0,"34":4,"35":0.0,"36":5,"37":7,"38":1,"39":4,"16":9,"40":0,"21":23,"4":1,"8":43,"17":9,"2":2,"3":1,"22":2,"1":1}
+},
+{
+"name": "GenericRobot","key": "m","value":145, 
+"metrics":{"23":2,"24":1,"25":1,"26":1,"27":2,"28":2,"29":1,"0":2,"30":1,"31":1,"32":5,"33":1,"34":1,"35":2,"36":1,"37":1,"38":1,"39":1,"16":2,"40":1,"4":2,"8":2,"17":1,"2":2,"3":2,"1":2},
+"metricvalues":{"23":7,"24":0,"25":1,"26":0.0,"27":143,"28":4,"29":1,"0":2,"30":13,"31":37,"32":1.0,"33":0.0,"34":0,"35":0.667,"36":2,"37":28,"38":0,"39":5,"16":49,"40":0,"21":139,"4":2,"8":145,"17":16,"2":2,"3":2,"22":22,"1":2}
+},
+{
+"name": "Muckraker","key": "n","value":116, 
+"metrics":{"23":2,"24":1,"25":1,"26":1,"27":2,"28":1,"29":2,"0":2,"30":1,"31":1,"32":5,"33":4,"34":1,"35":1,"36":1,"37":1,"38":1,"39":2,"16":2,"40":1,"4":2,"8":2,"17":2,"2":2,"3":1,"1":2},
+"metricvalues":{"23":8,"24":0,"25":11,"26":0.0,"27":104,"28":0,"29":2,"0":2,"30":4,"31":39,"32":1.0,"33":0.909,"34":0,"35":0.5,"36":2,"37":32,"38":0,"39":6,"16":46,"40":0,"21":95,"4":2,"8":116,"17":40,"2":2,"3":1,"22":38,"1":2}
+}
+]
+},{
+"name": "examplefuncsplayer", "key": "q", "value":89, 
+"pmetrics":{"4":1,"12":1,"13":1,"14":1,"3":1,"1":1,"0":1,"6":1,"8":1,"2":1,"15":1,"16":1,"17":1},
+"pmetricvalues":{"4":1,"12":1,"13":0,"14":1,"3":1,"1":1,"0":1,"18":0.0,"6":1,"19":0.0,"8":89,"20":1.0,"2":1,"15":0,"16":28,"21":0,"17":0,"22":0},
+"children":[
+{
+"name": "RobotPlayer","key": "r","value":89, 
+"metrics":{"23":2,"24":1,"25":1,"26":1,"27":2,"28":1,"29":1,"0":2,"30":1,"31":1,"32":1,"33":4,"34":1,"35":1,"36":1,"37":1,"38":1,"39":2,"16":2,"40":1,"4":2,"8":2,"2":2,"3":1,"1":2},
+"metricvalues":{"23":6,"24":0,"25":0,"26":0.0,"27":70,"28":0,"29":1,"0":2,"30":0,"31":29,"32":0.0,"33":0.833,"34":5,"35":0.583,"36":0,"37":28,"38":8,"39":6,"16":28,"40":0,"4":2,"8":89,"2":2,"3":1,"1":2}
+}
+]
+}]
+ }
+;
+return ret;
+}
+var EQ_METRIC_MAP = {};
+EQ_METRIC_MAP["C3"] =0;
+EQ_METRIC_MAP["Complexity"] =1;
+EQ_METRIC_MAP["Coupling"] =2;
+EQ_METRIC_MAP["Lack of Cohesion"] =3;
+EQ_METRIC_MAP["Size"] =4;
+EQ_METRIC_MAP["Number of Highly Problematic Classes"] =5;
+EQ_METRIC_MAP["Number of Entities"] =6;
+EQ_METRIC_MAP["Number of Problematic Classes"] =7;
+EQ_METRIC_MAP["Class Lines of Code"] =8;
+EQ_METRIC_MAP["Number of External Packages"] =9;
+EQ_METRIC_MAP["Number of Packages"] =10;
+EQ_METRIC_MAP["Number of External Entities"] =11;
+EQ_METRIC_MAP["Efferent Coupling"] =12;
+EQ_METRIC_MAP["Number of Interfaces"] =13;
+EQ_METRIC_MAP["Number of Classes"] =14;
+EQ_METRIC_MAP["Afferent Coupling"] =15;
+EQ_METRIC_MAP["Weighted Method Count"] =16;
+EQ_METRIC_MAP["Coverage"] =17;
+EQ_METRIC_MAP["Normalized Distance"] =18;
+EQ_METRIC_MAP["Abstractness"] =19;
+EQ_METRIC_MAP["Instability"] =20;
+EQ_METRIC_MAP["TC Line Count"] =21;
+EQ_METRIC_MAP["TC Covered Line Count"] =22;
+EQ_METRIC_MAP["Coupling Between Object Classes"] =23;
+EQ_METRIC_MAP["Access to Foreign Data"] =24;
+EQ_METRIC_MAP["Number of Fields"] =25;
+EQ_METRIC_MAP["Specialization Index"] =26;
+EQ_METRIC_MAP["Class-Methods Lines of Code"] =27;
+EQ_METRIC_MAP["Number of Children"] =28;
+EQ_METRIC_MAP["Depth of Inheritance Tree"] =29;
+EQ_METRIC_MAP["Number of Methods"] =30;
+EQ_METRIC_MAP["Response For a Class"] =31;
+EQ_METRIC_MAP["Lack of Tight Class Cohesion"] =32;
+EQ_METRIC_MAP["Lack of Cohesion of Methods"] =33;
+EQ_METRIC_MAP["Number of Static Fields"] =34;
+EQ_METRIC_MAP["Lack of Cohesion Among Methods(1-CAM)"] =35;
+EQ_METRIC_MAP["CBO App"] =36;
+EQ_METRIC_MAP["Simple Response For a Class"] =37;
+EQ_METRIC_MAP["Number of Static Methods"] =38;
+EQ_METRIC_MAP["CBO Lib"] =39;
+EQ_METRIC_MAP["Number of Overridden Methods"] =40;
+var EQ_SELECTED_CLASS_METRIC 		= "Coupling";
+var EQ_SELECTED_PACKAGE_METRIC 	= "Coupling";
+var EQ_SELECTED_PROJECT_METRIC 	= "Class Lines of Code";
+var EQ_CLASS_METRIC_INDEX 	= EQ_METRIC_MAP[EQ_SELECTED_CLASS_METRIC];
+var EQ_PACKAGE_METRIC_INDEX	= EQ_METRIC_MAP[EQ_SELECTED_PACKAGE_METRIC];
+var EQ_PROJECT_METRIC_INDEX 	= EQ_METRIC_MAP[EQ_SELECTED_PROJECT_METRIC];
+var EQ_COLOR_OF_LEVELS = ["#1F77B4","#007F24","#62BF18","#FFC800","#FF5B13","#E50000"];
+var EQ_CLASS_METRICS = ["C3","Complexity","Coupling","Lack of Cohesion","Size","Class Lines of Code","Weighted Method Count","Coverage","TC Line Count","TC Covered Line Count","Coupling Between Object Classes","Access to Foreign Data","Number of Fields","Specialization Index","Class-Methods Lines of Code","Number of Children","Depth of Inheritance Tree","Number of Methods","Response For a Class","Lack of Tight Class Cohesion","Lack of Cohesion of Methods","Number of Static Fields","Lack of Cohesion Among Methods(1-CAM)","CBO App","Simple Response For a Class","Number of Static Methods","CBO Lib","Number of Overridden Methods"];
+var EQ_PACKAGE_METRICS = ["C3","Complexity","Coupling","Lack of Cohesion","Size","Number of Entities","Class Lines of Code","Efferent Coupling","Number of Interfaces","Number of Classes","Afferent Coupling","Weighted Method Count","Coverage","Normalized Distance","Abstractness","Instability","TC Line Count","TC Covered Line Count"];
+var EQ_PROJECT_METRICS = ["Number of Highly Problematic Classes","Number of Entities","Number of Problematic Classes","Class Lines of Code","Number of External Packages","Number of Packages","Number of External Entities"];
+function EQ_GET_COLOR(d) {
+if(d.metrics)
+return EQ_COLOR_OF_LEVELS[d.metrics[EQ_CLASS_METRIC_INDEX]];
+if(d.pmetrics)
+return EQ_COLOR_OF_LEVELS[d.pmetrics[EQ_PACKAGE_METRIC_INDEX]];
+if(d.prmetrics)
+return EQ_COLOR_OF_LEVELS[d.prmetrics[EQ_PROJECT_METRIC_INDEX]];
+return EQ_COLOR_OF_LEVELS[0];
+}
